bf0ab0b39358a54622560157111ec6105ea7c62e
frankworks <frankfax@hotmail.com>
2015-08-21 10:37:44 -0700

    SB Admin2  & Kendo
    8/21/2015
    Grid with AutoComplete from Northwind

---
+++ Controllers/NavbarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+using Kendo.Mvc.Grid.CRUD.Models;
+using Kendo.Mvc.Grid.CRUD.Domain;
+
+namespace Kendo.Mvc.Grid.CRUD.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Navbar(string controller, string action)
+        {
+            var data = new Data();
+
+            var navbar = data.itemsPerUser(controller, action, User.Identity.Name);
+
+            return PartialView("_navbar", navbar);
+        }
+    }
+}
---
+++ Models/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kendo.Mvc.Grid.CRUD.Models
+{
+    public class Roles
+    {
+        public int rowid { get; set; }
+        public int idUser { get; set; }
+        public int idMenu { get; set; }
+        public bool status { get; set; }
+    }
+}
